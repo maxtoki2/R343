cfe4cb05bc26efaf2fc93e283a54f3c46140ceff
Marchi <mmarchi@indians.com>
2023-09-12 08:11:04 -0400

    not sourcing

--- R/run_everything.r
+++ R/run_everything.r
@@ -1,2 +1,5 @@
-source("hello.R")
-hello()
+# source("R/hello.R")
+# hello()
+
+x <- glue("Hello, world! Today is {Sys.Date()}")
+save(x, file = paste0("data-raw/data_", make.names(Sys.time()), ".Rda"))
