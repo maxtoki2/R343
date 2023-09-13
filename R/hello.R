5bf3a9b77469551da9fc5c1559cd8cbb65c8d822
Marchi <mmarchi@indians.com>
2023-09-13 05:24:50 -0400

    took out the use of glue

--- R/hello.R
+++ R/hello.R
@@ -14,6 +14,6 @@
 #   Test Package:              'Ctrl + Shift + T'
 
 hello <- function() {
-  x <- glue("Hello, world! Today is {Sys.Date()}")
+  x <- paste0("Hello, world! Today is ", Sys.Date())
   save(x, file = paste0("data-raw/data_", make.names(Sys.time()), ".Rda"))
 }
