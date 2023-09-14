581fc6268083a5ca410d3f4a59b76109f4c23971
Marchi <mmarchi@indians.com>
2023-09-14 08:14:27 -0400

    does regular ggplot work?

--- R/app.R
+++ R/app.R
@@ -1,12 +1,19 @@
-library(gt)
+library(ggplot2)
+library(dplyr)
 
 ui <- fluidPage(
   "Hello, world!"
-  , gt_output(outputId = "table")
+  # , gt_output(outputId = "table")
+  , plotOutput("explot")
 )
 server <- function(input, output, session) {
-  output$table <- render_gt({
-    iris %>% gt()
-  })
+  # output$table <- render_gt({
+  #   iris %>% gt()
+  # })
+  output$explot <- renderPlot(
+    iris %>%
+      ggplot(aes(x = Sepal.Length, y = Sepal.Width, col = Species)) +
+      geom_point()
+  )
 }
 shinyApp(ui, server)
