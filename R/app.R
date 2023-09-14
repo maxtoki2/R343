a1016bf832834244116d393b743e926521ad8efd
Marchi <mmarchi@indians.com>
2023-09-14 08:10:48 -0400

    does gt work?

--- R/app.R
+++ R/app.R
@@ -1,6 +1,12 @@
+library(gt)
+
 ui <- fluidPage(
   "Hello, world!"
+  , gt_output(outputId = "table")
 )
 server <- function(input, output, session) {
+  output$table <- render_gt({
+    iris %>% gt()
+  })
 }
 shinyApp(ui, server)
